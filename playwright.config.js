435116fb0b8857139d895271ff232ee095b71605
Mukesh Otwani <mukeshotwani.50@gmail.com>
2026-04-07 20:52:26 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,7 @@ const baseurl=process.env[envkey]
 
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: './testcases',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
