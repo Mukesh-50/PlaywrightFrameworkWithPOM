53a49f6ca57a33d078fdda70e66ccad8ba2208ce
Mukesh Otwani <mukeshotwani.50@gmail.com>
2026-04-07 20:58:40 +0530

    Update SampleTest.spec.js

--- tests/SampleTest.spec.js
+++ tests/SampleTest.spec.js
@@ -6,5 +6,7 @@ test("Sample Test",async ({page})=>
     console.log("Sample2")
     console.log("Sample3")
     console.log("Sample4")
-
+    console.log("Sample5")
+    console.log("Sample6")
+    console.log("Sample7")
 })
