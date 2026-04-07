961e0e13c0eb9d605a299d6de669a3ad9f1ce479
Mukesh Otwani <mukeshotwani.50@gmail.com>
2026-04-07 20:56:14 +0530

    Update SampleTest.spec.js

--- tests/SampleTest.spec.js
+++ tests/SampleTest.spec.js
@@ -2,7 +2,9 @@ import {test,expect} from "@playwright/test"
 
 test("Sample Test",async ({page})=>
 {
-
-    
+    console.log("Sample1")
+    console.log("Sample2")
+    console.log("Sample3")
+    console.log("Sample4")
 
 })
